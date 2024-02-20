8b344c9aedf8d50573d107267d024039326b967c
maheye <u277721157@byui.edu>
2024-02-20 16:06:33 +0800

    updates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dotenv = require('dotenv').config()
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const passport = require('passport');
